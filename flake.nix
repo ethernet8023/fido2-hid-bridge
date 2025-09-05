161936359fbcf195d262286c16c9fd602482b819
Ari Lotter <arilotter@gmail.com>
2025-09-05 10:42:15 -0400

    fix on newer nixpkgs :)

--- flake.nix
+++ flake.nix
@@ -9,8 +9,15 @@
     };
   };
 
-  outputs = { self, nixpkgs, poetry2nix }:
-    let system = "x86_64-linux"; in
+  outputs =
+    {
+      self,
+      nixpkgs,
+      poetry2nix,
+    }:
+    let
+      system = "x86_64-linux";
+    in
     let
       # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
       pkgs = nixpkgs.legacyPackages.${system};
@@ -20,42 +27,66 @@
       packages.x86_64-linux = {
         fido2-hid-bridge = mkPoetryApplication {
           projectDir = self;
-          overrides = defaultPoetryOverrides.extend
-            (self: super: {
-              uhid = super.uhid.overridePythonAttrs (
-                old: {
-                  buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
-                }
-              );
-            });
+          overrides = defaultPoetryOverrides.extend (
+            self: super: {
+              uhid = super.uhid.overridePythonAttrs (old: {
+                buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+              });
+              cryptography = super.cryptography.overridePythonAttrs (old: {
+                cargoDeps =
+                  let
+                    name = "${old.pname}-${old.version}";
+                  in
+                  pkgs.rustPlatform.fetchCargoVendor {
+                    inherit name;
+                    inherit (old) src;
+                    sourceRoot = "${name}/src/rust";
+                    hash = "sha256-lYLkes5lwS6qVPRFTfNWGEvg35qnkpOR+Sp0Tto4gSI=";
+                  };
+              });
+            }
+          );
         };
         default = self.packages.${system}.fido2-hid-bridge;
       };
 
-      devShells.default = pkgs.mkShell {
+      devShells."x86_64-linux".default = pkgs.mkShell {
         inputsFrom = [ self.packages.${system}.fido2-hid-bridge ];
         packages = [ pkgs.poetry ];
       };
 
-      nixosModule = { config, lib, pkgs, ... }: with lib; let
-        cfg = config.services.fido2-hid-bridge;
-      in
-      {
-        options.services.fido2-hid-bridge = {
-          enable = lib.mkEnableOption "enable the fido2-hid-bridge service";
-        };
+      nixosModules.default =
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        let
+          cfg = config.services.fido2-hid-bridge;
+        in
+        {
+          options.services.fido2-hid-bridge = {
+            enable = lib.mkEnableOption "enable the fido2-hid-bridge service";
+          };
 
-        config = lib.mkIf cfg.enable {
-          systemd.services.fido2-hid-bridge = {
-            description = "FIDO2 to HID bridge";
-            after = [ "auditd.service" "syslog.target" "network.target" "local-fs.target" "pcscd.service" ];
-            wantedBy = [ "multi-user.target" ];
-            serviceConfig = {
-              Type = "simple";
-              ExecStart = "${self.packages.${system}.fido2-hid-bridge}/bin/fido2-hid-bridge";
+          config = lib.mkIf cfg.enable {
+            systemd.services.fido2-hid-bridge = {
+              description = "FIDO2 to HID bridge";
+              after = [
+                "auditd.service"
+                "syslog.target"
+                "network.target"
+                "local-fs.target"
+                "pcscd.service"
+              ];
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig = {
+                Type = "simple";
+                ExecStart = "${self.packages.${system}.fido2-hid-bridge}/bin/fido2-hid-bridge";
+              };
             };
           };
         };
-      };
     };
 }
